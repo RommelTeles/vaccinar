be9a05d710bffe0b3f6ec4a0e65f1f722f09be62
Rommel S Teles <rommelsteles@Rommels-MBP.home>
2022-02-04 23:25:32 +0000

    Initial Commit

---
+++ src/main/java/com/rommelteles/vaccinar/VaccinarApplication.java
@@ -0,0 +1,13 @@
+package com.rommelteles.vaccinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rommelteles/vaccinar/VaccinarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rommelteles.vaccinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
